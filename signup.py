72b82d851e3c491b1b76388b5e3cffe13b86d9ed
Sharvari-21 <2022.sharvari.more@ves.ac.in>
2024-01-29 00:01:23 +0530

    window

--- signup.py
+++ signup.py
@@ -1,1 +1,18 @@
 #This is our sign up page
+from tkinter import *
+from tkinter import messagebox
+import ast
+
+window=Tk()
+window.title("SignUp")
+window.geometry('925x500+300+200')
+window.configure(bg='#fff')
+window.resizable(False,False)
+
+window.mainloop()
+
+img = PhotoImage(file='Signup.jpg')
+Label(window,image=img,border=0,bg='white').place(x=50,y=90)
+
+frame=Frame(window,width=350,height=390,bg='#fff')
+frame.place(x=480,y=50)
