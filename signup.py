763cece1f1eecc1cc2d4a8095a5c7a9bb7f4e353
Sharvari-21 <2022.sharvari.more@ves.ac.in>
2024-01-29 00:09:19 +0530

    windows1

--- signup.py
+++ signup.py
@@ -1,5 +1,5 @@
 #This is our sign up page
-<<<<<<< HEAD
+
 from tkinter import *
 from tkinter import messagebox
 import ast
@@ -17,6 +17,6 @@ Label(window,image=img,border=0,bg='white').place(x=50,y=90)
 
 frame=Frame(window,width=350,height=390,bg='#fff')
 frame.place(x=480,y=50)
-=======
+
 #Jai Shree Ram   Datta Guru
->>>>>>> 257c4a220fcac0a2f2dced582901cd9a11fc2a72
+
