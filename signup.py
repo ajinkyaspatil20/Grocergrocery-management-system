be9b74ce697b6ba1676a713b263da72b04395f2a
Sharvari-21 <2022.sharvari.more@ves.ac.in>
2024-01-30 01:49:12 +0530

    signup2

--- signup.py
+++ signup.py
@@ -11,6 +11,13 @@ window.geometry('925x500+300+200')
 window.configure(bg='#fff')
 window.resizable(False,False)
 
+def signin():
+    username=user.get()
+    password=code.get()
+
+    if username=='admin' and password=='1234':
+        print('ASYA')
+
 
 bgOriginal = Image.open('Signup.jpg').resize((925,500))
 # bgImage = ImageTk.PhotoImage(bgOriginal)
@@ -20,12 +27,50 @@ img =ImageTk.PhotoImage(bgOriginal)
 Label(window,image=img,border=0,bg='white').place(x=0,y=0)
 
 frame=Frame(window,width=350,height=350,bg='white')
-frame.place(x=500,y=50)
+frame.place(x=500,y=65)
 
-heading=Label(frame,text='Sign in',fg='#57a1f8',bg='white',font=('Microsoft Yahei UI',23,'bold'))
-heading.grid(row=0,column=0)
+heading=Label(frame,text='Sign in',fg='#34e4e1',bg='white',font=('Microsoft Yahei UI',23,'bold'))
+heading.place(x=120,y=30)
 
-user = Entry(frame,width=35,fg='black',border=2,bg="white",font=('Microsoft Yahei UI',11))
+def on_enter(e):
+    user.delete(0,'end')
+
+def on_leave(e):
+    name=user.get()
+    if name=='':
+        user.insert(0,'Username')
+
+user = Entry(frame,width=30,fg='black',border=2,bg="white",font=('Microsoft Yahei UI',11))
+user.place(x=30,y=80)
+user.insert(0,'Username')
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
+Frame(frame,width=295,height=2,bg='black').place(x=25,y=107)
+
+def on_enter(e):
+   code.delete(0,'end')
+
+def on_leave(e):
+    name=user.get()
+    if name=='':
+        code.insert(0,'Password')
+
+code = Entry(frame,width=30,fg='black',border=2,bg="white",font=('Microsoft Yahei UI',11))
+code.place(x=30,y=150)
+code.insert(0,'Password')
+code.bind('<FocusIn>', on_enter)
+code.bind('<FocusOut>', on_leave)
+
+Frame(frame,width=295,height=2,bg='black').place(x=25,y=177)
+
+Button(frame,width=39,pady=7,text='Sign in',bg='#34e4e1',fg='white',border=0).place(x=35,y=204)
+label=Label(frame,text="Don't have an account?",fg='black',bg='white',font=('Microsoft Yahei UI',9))
+label.place(x=75,y=270)
+
+sign_up=Button(frame,width=6,text='Sign Up',border=0,bg='white',cursor='hand2',fg='#34e4e1',command=signin)
+sign_up.place(x=215,y=270)
+
+
 
 
 
